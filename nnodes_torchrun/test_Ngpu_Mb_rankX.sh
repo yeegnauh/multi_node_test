a23679b82e3e71000ada434be011642247c48350
yeegnauh <395682039@qq.com>
2023-08-22 16:12:39 +0800

    fix

--- nnodes_torchrun/test_Ngpu_Mb_rankX.sh
+++ nnodes_torchrun/test_Ngpu_Mb_rankX.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#sh test_Ngpu_Mb_rankX.sh $RANK 32 13
+#sh test_Ngpu_Mb_rankX.sh $RANK 32 13 1
 rankX=$1
 Ngpu=$2
 Mb=$3
@@ -7,14 +7,15 @@ Mb=$3
 log_dir="log_${Ngpu}gpu_${Mb}B"
 mkdir -p $log_dir
 conf_dir="config_${Ngpu}gpu_${Mb}B"
+nnode=`expr $Ngpu / 8`
 
-config_ids=(1 2 3)
+config_ids=( $4)
 for n in ${config_ids[@]};do
 #for n in {1..3};do
     conf_file=./${conf_dir}/${Mb}B_sft_$n.py
     log_file=$log_dir/${Mb}B_sft_$n.log
     ls ${conf_file} 2>&1 | tee ${log_file}
     echo `grep -E "checkpoint=|micro_num=|micro_bsz=|zero1=|pipeline=|tensor=" ${conf_file}` 2>&1 | tee -a  ${log_file}
-    torchrun --nnodes=4 --nproc_per_node=8 --node_rank=$1 --master_addr=10.198.34.129 --master_port=29600 train.py --config ${conf_file} --launcher "torch" 2>&1 | tee -a ${log_file}
+    torchrun --nnodes=$nnode --nproc_per_node=8 --node_rank=$1 --master_addr=10.105.10.10 --master_port=29600 train.py --config ${conf_file} --launcher "torch" 2>&1 | tee -a ${log_file}
     sleep 10
 done
--- tmux_tools/readme.md
+++ tmux_tools/readme.md
@@ -1,7 +1,20 @@
-1. append ~/.ssh/config
-2. test if it works: 
-./jump node1
-3. write node_list file
-4. create a tmux window, run it:
+1. append ~/.ssh/config  
+
+2. ssh machine  
+
+2.1 jump -> target machine, test if it works, enter password and verification code manually once, so that no need to input again before the expiration date:  
+ssh hy-jump-control  
+
+then, test if it works:   
+./jump node1  
+
+2.2 login target machine through plaintext password:  
+expect login node1  
+
+3. write node_list file  
+
+4. create a tmux window, run it:  
 ./jump_control node_list
+
+replace the `jump` in `jump_control` with `login` as appropriate.
 
